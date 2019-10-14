b61b7c8cb840928897433e1b51e80d34178b83bf
Aleksey <pro.invader73@gmail.com>
2019-10-14 20:37:56 +0400

    On process

--- app/src/main/java/com/alekseymakarov/multinews/ui/NewsActivity.kt
+++ app/src/main/java/com/alekseymakarov/multinews/ui/NewsActivity.kt
@@ -1,12 +1,13 @@
-package com.alekseymakarov.multinews
+package com.alekseymakarov.multinews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alekseymakarov.multinews.R
 
 class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
